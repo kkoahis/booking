116341d02eff0afb707c5886d525bf02753a9025
kkoahis <khoakun27@gmail.com>
2023-01-13 23:47:26 +0700

    done footer

---
+++ youtube2022/src/components/footer/Footer.jsx
@@ -0,0 +1,73 @@
+import "./Footer.css"
+
+const Footer = () => {
+   return (
+      <div className="footer">
+         <div className="footerLists">
+            <ul className="footerList">
+               <li className="footerListItem">Countries</li>
+               <li className="footerListItem">Regions</li>
+               <li className="footerListItem">Cities</li>
+               <li className="footerListItem">Districts</li>
+               <li className="footerListItem">Airports</li>
+               <li className="footerListItem">Hotels</li>
+               <li className="footerListItem">Places of interest</li>
+            </ul>
+            <ul className="footerList">
+               <li className="footerListItem">Homes</li>
+               <li className="footerListItem">Apartments</li>
+               <li className="footerListItem">Resorts</li>
+               <li className="footerListItem">Villas</li>
+               <li className="footerListItem">Hostels</li>
+               <li className="footerListItem">B&Bs</li>
+               <li className="footerListItem">Guest houses</li>
+            </ul>
+            <ul className="footerList">
+               <li className="footerListItem">Unique places to stay</li>
+               <li className="footerListItem">All destinations</li>
+               <li className="footerListItem">All flight destinations</li>
+               <li className="footerListItem">Discover</li>
+               <li className="footerListItem">Reviews</li>
+               <li className="footerListItem">Unpacked: Travel articles</li>
+               <li className="footerListItem">Travel Communities</li>
+               <li className="footerListItem">Seasonal and holiday deals</li>
+            </ul>
+            <ul className="footerList">
+               <li className="footerListItem">Countries</li>
+               <li className="footerListItem">Regions</li>
+               <li className="footerListItem">Cities</li>
+               <li className="footerListItem">Districts</li>
+               <li className="footerListItem">Airports</li>
+               <li className="footerListItem">Hotels</li>
+            </ul>
+            <ul className="footerList">
+               <li className="footerListItem">Car hire</li>
+               <li className="footerListItem">Flight finder</li>
+               <li className="footerListItem">Restaurant reservations</li>
+               <li className="footerListItem">BookingH.com for Travel Agents</li>
+            </ul>
+            <ul className="footerList">
+               <li className="footerListItem">Coronavirus (COVID-19) FAQs</li>
+               <li className="footerListItem">About Booking.com</li>
+               <li className="footerListItem">Customer Service help</li>
+               <li className="footerListItem">Partner help</li>
+               <li className="footerListItem">Careers</li>
+               <li className="footerListItem">Sustainability</li>
+               <li className="footerListItem">Press centre</li>
+               <li className="footerListItem">Safety resource centre</li>
+               <li className="footerListItem">Investor relations</li>
+               <li className="footerListItem">Terms & Conditions</li>
+               <li className="footerListItem">Partner dispute</li>
+               <li className="footerListItem">How we work</li>
+               <li className="footerListItem">Privacy & Cookie Statement</li>
+               <li className="footerListItem">MSA Statement</li>
+               <li className="footerListItem">Corporate contact</li>
+            </ul>
+         </div>
+         <div className="footerText">Copyright © 1996–2023 BookingH.com™. All rights reserved.
+         </div>
+      </div>
+   )
+}
+
+export default Footer
