2dead2be5d06fd8682962c0fd2404d363ca56dd5
kkoahis <khoakun27@gmail.com>
2023-01-15 10:20:04 +0700

    done Search-input in bookingH/Hotels

--- youtube2022/src/components/header/Header.jsx
+++ youtube2022/src/components/header/Header.jsx
@@ -6,8 +6,11 @@ import 'react-date-range/dist/theme/default.css'; // theme css file
 import { DateRange } from "react-date-range";
 import { useState } from "react";
 import { format } from "date-fns";
+import { useNavigate } from "react-router-dom";
 
-const Header = ({type}) => {
+const Header = ({ type }) => {
+  const [destination, setDestination] = useState("");
+
   const [openDate, setOpenDate] = useState(false);
   const [date, setDate] = useState([
     {
@@ -24,6 +27,8 @@ const Header = ({type}) => {
     room: 1
   })
 
+  const navigate = useNavigate()
+
   const handleOption = (name, operation) => {
     setOptions(prev => {
       return {
@@ -32,6 +37,9 @@ const Header = ({type}) => {
     })
   }
 
+  const handleSearch = () => {
+    navigate("/hotels", { state: { destination, date, options } })
+  }
 
   return (
     <div className="header">
@@ -67,7 +75,7 @@ const Header = ({type}) => {
             <div className="headerSearch">
               <div className="headerSearchItem">
                 <FontAwesomeIcon icon={faBed} className="headerIcon" />
-                <input type="text" placeholder="Where are you going?" className="headerSearchInput" />
+                <input type="text" placeholder="Where are you going?" className="headerSearchInput" onChange={e => setDestination(e.target.value)} />
               </div>
               <div className="headerSearchItem">
                 <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
@@ -78,6 +86,7 @@ const Header = ({type}) => {
                   moveRangeOnFirstSelection={false}
                   ranges={date}
                   className="date"
+                  minDate={new Date()}
                 />}
               </div>
               <div className="headerSearchItem">
@@ -111,7 +120,7 @@ const Header = ({type}) => {
                 </div>}
               </div>
               <div className="headerSearchItem">
-                <button className="headerBtn">Search</button>
+                <button className="headerBtn" onClick={handleSearch}>Search</button>
               </div>
             </div></>}
       </div>
--- youtube2022/src/page/list/List.jsx
+++ youtube2022/src/page/list/List.jsx
@@ -1,14 +1,83 @@
-import React from 'react'
+import "./List.css"
 import Header from '../../components/header/Header'
 import Navbar from '../../components/navbar/Navbar'
+import { useLocation } from "react-router-dom"
+import { useState } from "react"
+import { format } from "date-fns"
+import { DateRange } from "react-date-range"
 
 
 const List = () => {
+
+  const location = useLocation();
+  const [destination, setDestination] = useState(location.state.destination);
+  const [date, setDate] = useState(location.state.date);
+  const [openDate, setOpenDate] = useState(false);
+  const [options, setOptions] = useState(location.state.options);
+
   return (
     <div>
       <Navbar />
       <Header type="list" />
+      <div className="listContainer">
+        <div className="listWrapper">
+          <div className="listSearch">
+            <h1 className="listSearch__Title">Search</h1>
+            <div className="listSearch__Item">
+              <label>Destination</label>
+              <input placeholder={destination} type="text" />
+            </div>
+            <div className="listSearch__Item">
+              <label>Check-in Date</label>
+              <span onClick={() => setOpenDate(!openDate)}>
+                {`${format(date[0].startDate, "dd/MM/yyyy")} to ${format(date[0].endDate, "dd/MM/yyyy")}`}
+              </span>
+              {openDate && (<DateRange
+                onChange={item => setDate([item.selection])} minDate={new Date()} ranges={date}
+              />)}
+            </div>
+            <div className="listSearch__Item">
+              <label>Options</label>
+              <div className="listOption">
+
+                <div className="listOptionItem">
+                  <span className="listOptionItem__Text">
+                    Min price <small>per night</small>
+                  </span>
+                  <input type="number" className="listOptionItem__input" />
+                </div>
+                <div className="listOptionItem">
+                  <span className="listOptionItem__Text">
+                    Max price <small>per night</small>
+                  </span>
+                  <input type="number" className="listOptionItem__input" />
+                </div>
+                <div className="listOptionItem">
+                  <span className="listOptionItem__Text">
+                    Adult
+                  </span>
+                  <input type="number" min={1} className="listOptionItem__input" placeholder={options.adult} />
+                </div>
+                <div className="listOptionItem">
+                  <span className="listOptionItem__Text">
+                    Children
+                  </span>
+                  <input type="number" min={0} className="listOptionItem__input" placeholder={options.children} />
+                </div>
+                <div className="listOptionItem">
+                  <span className="listOptionItem__Text">
+                    Room
+                  </span>
+                  <input type="number" min={1} className="listOptionItem__input" placeholder={options.room} />
+                </div>
+              </div>
+            </div>
+            <button>Search</button>
+          </div>
+          <div className="listResult"></div>
+        </div>
       </div>
+    </div>
   )
 }
 
